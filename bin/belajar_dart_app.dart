7193e8ca9df1f8bb19e76ee977b6fe8d08816ddc
epul2207 <epuls073@gmail.com>
2023-09-05 18:41:39 +0700

    Membuat app

---
+++ bin/belajar_dart_app.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_app/belajar_dart_app.dart' as belajar_dart_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_app.calculate()}!');
+}
---
+++ test/belajar_dart_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_app/belajar_dart_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
